e36579cdce919241950b654027b0e7352c3baac0
Favour Ori <orifavour2000@gmail.com>
2021-04-23 04:55:21 +0200

    setup env, cors and bodyparser

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 express = require('express')
 app = express()
+require('dotenv').config()
+
+
+//setup body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+let cors = require("cors");
+app.use(cors());
+
+//allowing cors
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 
 app.get("/", (req, res)=>{
