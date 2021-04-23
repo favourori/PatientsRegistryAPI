e5b0b27c23ad39293461e380a88a4b7a8e07a93e
Favour Ori <orifavour2000@gmail.com>
2021-04-23 11:05:53 +0200

    updated port

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 });
 
 // Spin up dev server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
