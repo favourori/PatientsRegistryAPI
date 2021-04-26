13b8bd8d3605eac754c667b7ba88c102607f9e05
Fawas kareem <phawazzzy@gmail.com>
2021-04-26 11:53:58 +0100

    patients and number of patients with a disease

--- route/disease.js
+++ route/disease.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { createDisease, getDisease, getDiseases } = require("../controller/disease");
+const {
+  createDisease,
+  getDisease,
+  getDiseases,
+  patients
+} = require("../controller/disease");
 const { mustBeLoggedIn } = require("../helpers/authHelper");
 const { diseaseCreate } = require("../helpers/validationSchema");
 const { validator } = require("../middlewares/validationMid");
@@ -9,5 +14,6 @@ const router = express.Router();
 router.post("/create", mustBeLoggedIn, validator(diseaseCreate), createDisease);
 router.get("/all", mustBeLoggedIn, getDiseases);
 router.get("/:id", mustBeLoggedIn, getDisease);
+router.get("/:diseaseId/patients", patients);
 
 module.exports = router;
